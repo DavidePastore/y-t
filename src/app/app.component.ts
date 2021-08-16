86f9b01911366c38e81b292092f51d4837680b88
Davide Pastore <pasdavide@gmail.com>
2021-08-16 13:01:16 +0200

    Add Sign-In view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    { title: 'Sign In', url: '/sign-in', icon: 'person' },
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
